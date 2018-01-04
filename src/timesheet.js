222f790fe67ce09ffe1a41f86ca3ff18794dab73
Markus Spanier <spanierm@gmx.net>
2018-01-05 00:48:22 +0100

    Add integration tests for timesheet logger

--- src/timesheet.js
+++ src/timesheet.js
@@ -1,7 +1,7 @@
-const moment = require("moment");
 const fs = require("fs");
+const moment = require("moment");
 
-TopSide = {
+const TOP_SIDE = {
   TOP: 0,
   COFFEE: 5,
   MAIL: 6,
@@ -34,41 +34,56 @@ TopSide = {
       case 4:
         return "C";
       case 9:
-        return "BOTTOM_TIP";
+        return "BOTTOM";
     }
   }
 };
 
-TimesheetTimeFormat = "HH:mm";
+const TIMESHEET_TIME_FORMAT = "HH:mm";
 
-_startTime = moment().format(TimesheetTimeFormat);
-_topSide = TopSide.TOP;
-
-_timesheetPath = `${moment().format("YYYY_MM/DD")}.times`;
-
-logChangesToTimesheet = (data, isNotification) => {
-  const endTime = moment().format(TimesheetTimeFormat);
-  fs.appendFileSync(_timesheetPath, `${startTime} - ${endTime}: top side was ${TopSide.of(topSide)}\n`);
-  _topSide = data.readUInt8(0);
-  _startTime = endTime;
-};
-
-getTimesheetTime = (someMoment = moment()) => {
+const getTimesheetTime = (someMoment = moment()) => {
   const timesheetMoment = _areMinutesDivisibleByThree(someMoment) ? someMoment : _roundMinutesUpToBeDivisibleByThree(someMoment);
-  return timesheetMoment.format(TimesheetTimeFormat)
+  return timesheetMoment.format(TIMESHEET_TIME_FORMAT)
 };
 
-_areMinutesDivisibleByThree = (timesheetMoment) => {
+const _areMinutesDivisibleByThree = (timesheetMoment) => {
   return timesheetMoment.minutes() % 3 === 0;
 };
 
-_roundMinutesUpToBeDivisibleByThree = (timesheetMoment) => {
+const _roundMinutesUpToBeDivisibleByThree = (timesheetMoment) => {
   const minutesModThree = timesheetMoment.minutes() % 3;
   return timesheetMoment.add(3 - minutesModThree, "minutes")
 };
 
+let lastChange;
+
+const logChangesToTimesheetCallback = (timesheetPath) => {
+  return (data, isNotification) => {
+    const currentTime = getTimesheetTime();
+    if (lastChange) {
+      if (_shouldNotTrack(lastChange.topSide)) {
+        fs.appendFileSync(timesheetPath, "\n");
+      }
+      else {
+        if (lastChange.time !== currentTime) {
+          fs.appendFileSync(timesheetPath, `${lastChange.time} - ${currentTime} top side was ${TOP_SIDE.of(lastChange.topSide)}\n`);
+        }
+      }
+    }
+    lastChange = {
+      time: currentTime,
+      topSide: data.readUInt8(0)
+    }
+  };
+};
+
+const _shouldNotTrack = (topSide) => {
+  return topSide === TOP_SIDE.TOP || topSide === TOP_SIDE.BOTTOM;
+};
+
 module.exports = {
-  TopSide,
-  logChangesToTimesheet,
-  getTimesheetTime
+  TIMESHEET_TIME_FORMAT,
+  TOP_SIDE,
+  getTimesheetTime,
+  logChangesToTimesheetCallback
 };
--- test/timesheetTest.js
+++ test/timesheetTest.js
@@ -1,12 +1,17 @@
+const fs = require("fs");
 const moment = require("moment");
-const should = require("should");
+const proxyquire = require("proxyquire");
+require("should");
+const sinon = require("sinon");
 const timesheet = require("../src/timesheet.js");
 const tmp = require("tmp");
+
 
 describe("calculate (rounded) time from moment", () => {
   describe("entries match HH:mm", () => {
     it("entries must match HH:mm", () => {
       const currentTime = timesheet.getTimesheetTime();
+
       currentTime.should.match(/\d{2}:\d{2}/);
     });
   });
@@ -14,7 +19,9 @@ describe("calculate (rounded) time from moment", () => {
   const checkCalculation = (currentHour, currentMinute, expectedTimesheetTime) => {
     it(`${currentHour}:${currentMinute} => ${expectedTimesheetTime}`, () => {
       const testMoment = moment().hour(currentHour).minute(currentMinute);
+
       const currentTime = timesheet.getTimesheetTime(testMoment);
+
       currentTime.should.be.equal(expectedTimesheetTime);
     })
   };
@@ -29,4 +36,100 @@ describe("calculate (rounded) time from moment", () => {
     checkCalculation(8, 59, "09:00");
     checkCalculation(23, 58, "00:00");
   });
+});
+
+describe("integration tests with temporary files", () => {
+  const doIntegrationTestWithTemporaryTimesheetFile = (testDescription, changesOfTimeularZei, expectedContent) => {
+    it(testDescription, () => {
+      const timesheetWithStubbedMomentLibrary = setupMomentStub(changesOfTimeularZei);
+      const {logChangesToTimesheetCallback, filename} = logChangesToTemporaryTimesheetCallback(timesheetWithStubbedMomentLibrary);
+
+      for (let i = 0; i < changesOfTimeularZei.length; i++) {
+        logChangesToTimesheetCallback(createTopSideDataBuffer(changesOfTimeularZei[i].topSide));
+      }
+
+      const actualConent = fs.readFileSync(filename).toString();
+      actualConent.should.equal(expectedContent);
+    });
+  };
+
+  const setupMomentStub = (changesOfTimeularZei) => {
+    const momentStub = sinon.stub();
+    for (let i = 0; i < changesOfTimeularZei.length; i++) {
+      momentStub.onCall(i).returns(changesOfTimeularZei[i].moment);
+    }
+    return proxyquire("../src/timesheet.js", {
+      "moment": momentStub
+    });
+  };
+
+  const logChangesToTemporaryTimesheetCallback = (timesheetWithStubbedMomentLibrary) => {
+    const temporaryFile = tmp.fileSync();
+    const logChangesToTimesheetCallback = timesheetWithStubbedMomentLibrary.logChangesToTimesheetCallback(temporaryFile.name);
+    return {
+      logChangesToTimesheetCallback,
+      filename: temporaryFile.name
+    }
+  };
+
+  const createTopSideDataBuffer = (topSideValue) => {
+    const buffer = new Buffer(1);
+    buffer.writeInt8(topSideValue);
+    return buffer;
+  };
+
+  describe("add empty line if Timeular Zei is in base", () => {
+    [timesheet.TOP_SIDE.TOP, timesheet.TOP_SIDE.BOTTOM].forEach((topSide) => {
+      doIntegrationTestWithTemporaryTimesheetFile(`top side is ${timesheet.TOP_SIDE.of(topSide)}`, [
+        {moment: moment().hour(11).minute(0), topSide: timesheet.TOP_SIDE.A},
+        {moment: moment().hour(12).minute(0), topSide: topSide},
+        {moment: moment().hour(13).minute(0), topSide: timesheet.TOP_SIDE.A},
+        {moment: moment().hour(14).minute(0), topSide: timesheet.TOP_SIDE.B},
+      ], "11:00 - 12:00 top side was A\n" +
+        "\n" +
+        "13:00 - 14:00 top side was A\n"
+      );
+    });
+
+  });
+
+  describe("skip entries if time did not change", () => {
+    doIntegrationTestWithTemporaryTimesheetFile("same minute", [
+      {moment: moment().hour(8).minute(30), topSide: timesheet.TOP_SIDE.MAIL},
+      {moment: moment().hour(8).minute(30), topSide: timesheet.TOP_SIDE.CHAT},
+      {moment: moment().hour(9).minute(30), topSide: timesheet.TOP_SIDE.MAIL}
+    ], "08:30 - 09:30 top side was CHAT\n");
+
+    doIntegrationTestWithTemporaryTimesheetFile("same minute after rounding", [
+      {moment: moment().hour(8).minute(28), topSide: timesheet.TOP_SIDE.MAIL},
+      {moment: moment().hour(8).minute(30), topSide: timesheet.TOP_SIDE.CHAT},
+      {moment: moment().hour(9).minute(29), topSide: timesheet.TOP_SIDE.MAIL},
+      {moment: moment().hour(9).minute(30), topSide: timesheet.TOP_SIDE.CLOCK},
+      {moment: moment().hour(10).minute(30), topSide: timesheet.TOP_SIDE.MAIL}
+    ], "08:30 - 09:30 top side was CHAT\n" +
+      "09:30 - 10:30 top side was CLOCK\n");
+  });
+
+  doIntegrationTestWithTemporaryTimesheetFile("end-to-end test", [
+    {moment: moment().hour(8).minute(0), topSide: timesheet.TOP_SIDE.MAIL},
+    {moment: moment().hour(8).minute(15), topSide: timesheet.TOP_SIDE.A},
+    {moment: moment().hour(9).minute(0), topSide: timesheet.TOP_SIDE.CLOCK},
+    {moment: moment().hour(9).minute(15), topSide: timesheet.TOP_SIDE.A},
+    {moment: moment().hour(9).minute(45), topSide: timesheet.TOP_SIDE.TOP},
+    {moment: moment().hour(10).minute(0), topSide: timesheet.TOP_SIDE.A},
+    {moment: moment().hour(12).minute(0), topSide: timesheet.TOP_SIDE.B},
+    {moment: moment().hour(13).minute(0), topSide: timesheet.TOP_SIDE.BOTTOM},
+    {moment: moment().hour(14).minute(0), topSide: timesheet.TOP_SIDE.B},
+    {moment: moment().hour(14).minute(0), topSide: timesheet.TOP_SIDE.C},
+    {moment: moment().hour(16).minute(0), topSide: timesheet.TOP_SIDE.A}
+  ], "08:00 - 08:15 top side was MAIL\n" +
+    "08:15 - 09:00 top side was A\n" +
+    "09:00 - 09:15 top side was CLOCK\n" +
+    "09:15 - 09:45 top side was A\n" +
+    "\n" +
+    "10:00 - 12:00 top side was A\n" +
+    "12:00 - 13:00 top side was B\n" +
+    "\n" +
+    "14:00 - 16:00 top side was C\n"
+  );
 });
