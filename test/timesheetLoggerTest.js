9a60e6257b261aadbbb9245d3aa405d942b435f2
Markus Spanier <spanierm@gmx.net>
2018-01-07 13:50:27 +0100

    Refactoring

--- test/timesheetLoggerTest.js
+++ test/timesheetLoggerTest.js
@@ -4,8 +4,9 @@ const path = require('path')
 const proxyquire = require('proxyquire')
 require('should')
 const sinon = require('sinon')
-const timesheetLogger = require('../src/timesheetLogger.js')
 const tmp = require('tmp')
+
+const timesheetLogger = require('../src/timesheetLogger.js')
 
 describe('calculate (rounded) time from moment', () => {
   describe('entries match HH:mm', () => {
@@ -16,8 +17,8 @@ describe('calculate (rounded) time from moment', () => {
     })
   })
 
-  const checkCalculation = (currentHour, currentMinute, expectedTimesheetTime) => {
-    it(`${currentHour}:${currentMinute} => ${expectedTimesheetTime}`, () => {
+  const checkTimesheetCalculation = (currentHour, currentMinute, expectedTimesheetTime) => {
+    it(`${currentHour.toString().padStart(2, '0')}:${currentMinute.toString().padStart(2, '0')} => ${expectedTimesheetTime}`, () => {
       const testMoment = moment().hour(currentHour).minute(currentMinute)
 
       const currentTime = timesheetLogger.getTimesheetTime(testMoment)
@@ -27,45 +28,45 @@ describe('calculate (rounded) time from moment', () => {
   }
 
   describe('minutes are divisible by three', () => {
-    checkCalculation(12, 0, '12:00')
-    checkCalculation(9, 11, '09:12')
-    checkCalculation(22, 52, '22:54')
+    checkTimesheetCalculation(12, 0, '12:00')
+    checkTimesheetCalculation(9, 11, '09:12')
+    checkTimesheetCalculation(22, 52, '22:54')
   })
 
   describe('hour is increased if rounded', () => {
-    checkCalculation(8, 59, '09:00')
-    checkCalculation(23, 58, '00:00')
+    checkTimesheetCalculation(8, 59, '09:00')
+    checkTimesheetCalculation(23, 58, '00:00')
   })
 })
 
 describe('integration tests with temporary files', () => {
   const doIntegrationTestWithTemporaryTimesheetFile = (testDescription, changesOfTimeularZei, expectedContent) => {
     it(testDescription, () => {
-      const timesheetWithStubbedMomentLibrary = _setupMomentStub(changesOfTimeularZei)
-      const {logChangesToTimesheetCallback, filename} = _logChangesToTemporaryTimesheetCallback(timesheetWithStubbedMomentLibrary)
+      const timesheetLoggerWithMomentStub = _createTimesheetLoggerWithMomentStub(changesOfTimeularZei)
+      const {logChangesToTimesheetCallback, filename} = _logChangesToTemporaryTimesheetCallback(timesheetLoggerWithMomentStub)
 
-      for (let i = 0; i < changesOfTimeularZei.length; i++) {
-        logChangesToTimesheetCallback(_createTopSideDataBuffer(changesOfTimeularZei[i].topSide))
-      }
+      changesOfTimeularZei.forEach(changeOfTimeularZei => {
+        logChangesToTimesheetCallback(_createTopSideDataBuffer(changeOfTimeularZei.topSide))
+      })
 
       const actualConent = fs.readFileSync(filename).toString()
       actualConent.should.equal(expectedContent)
     })
   }
 
-  const _setupMomentStub = (changesOfTimeularZei) => {
+  const _createTimesheetLoggerWithMomentStub = (changesOfTimeularZei) => {
     const momentStub = sinon.stub()
-    for (let i = 0; i < changesOfTimeularZei.length; i++) {
-      momentStub.onCall(i).returns(changesOfTimeularZei[i].moment)
-    }
+    changesOfTimeularZei.forEach((changeOfTimeularZei, index) => {
+      momentStub.onCall(index).returns(changeOfTimeularZei.moment)
+    })
     return proxyquire('../src/timesheetLogger.js', {
       'moment': momentStub
     })
   }
 
-  const _logChangesToTemporaryTimesheetCallback = (timesheetWithStubbedMomentLibrary) => {
+  const _logChangesToTemporaryTimesheetCallback = (timesheetLoggerWithMomentStub) => {
     const temporaryFile = tmp.fileSync()
-    const logChangesToTimesheetCallback = timesheetWithStubbedMomentLibrary.logChangesToTimesheetCallback(temporaryFile.name)
+    const logChangesToTimesheetCallback = timesheetLoggerWithMomentStub.logChangesToTimesheetCallback(temporaryFile.name)
     return {
       logChangesToTimesheetCallback,
       filename: temporaryFile.name
@@ -101,11 +102,11 @@ describe('integration tests with temporary files', () => {
 
     doIntegrationTestWithTemporaryTimesheetFile('same minute after rounding', [
       {moment: moment().hour(8).minute(28), topSide: timesheetLogger.TOP_SIDE.MAIL},
-      {moment: moment().hour(8).minute(30), topSide: timesheetLogger.TOP_SIDE.CHAT},
+      {moment: moment().hour(8).minute(30), topSide: timesheetLogger.TOP_SIDE.COFFEE},
       {moment: moment().hour(9).minute(29), topSide: timesheetLogger.TOP_SIDE.MAIL},
       {moment: moment().hour(9).minute(30), topSide: timesheetLogger.TOP_SIDE.CLOCK},
       {moment: moment().hour(10).minute(30), topSide: timesheetLogger.TOP_SIDE.MAIL}
-    ], '08:30 - 09:30 top side was CHAT\n' +
+    ], '08:30 - 09:30 top side was COFFEE\n' +
       '09:30 - 10:30 top side was CLOCK\n')
 
     doIntegrationTestWithTemporaryTimesheetFile('for breaks as well', [
@@ -129,8 +130,8 @@ describe('integration tests with temporary files', () => {
           {moment: moment().hour(8).minute(0), topSide: timesheetLogger.TOP_SIDE.MAIL},
           {moment: moment().hour(9).minute(0), topSide: timesheetLogger.TOP_SIDE.CLOCK}
         ]
-        const timesheetWithStubbedMomentLibrary = _setupMomentStub(changesOfTimeularZei)
-        const logChangesToTimesheetCallback = timesheetWithStubbedMomentLibrary.logChangesToTimesheetCallback(temporaryFilePath)
+        const timesheetLoggerWithStubbedMomentLibrary = _createTimesheetLoggerWithMomentStub(changesOfTimeularZei)
+        const logChangesToTimesheetCallback = timesheetLoggerWithStubbedMomentLibrary.logChangesToTimesheetCallback(temporaryFilePath)
 
         for (let i = 0; i < changesOfTimeularZei.length; i++) {
           logChangesToTimesheetCallback(_createTopSideDataBuffer(changesOfTimeularZei[i].topSide))
@@ -156,10 +157,11 @@ describe('integration tests with temporary files', () => {
     {moment: moment().hour(9).minute(15), topSide: timesheetLogger.TOP_SIDE.A},
     {moment: moment().hour(9).minute(45), topSide: timesheetLogger.TOP_SIDE.TOP},
     {moment: moment().hour(10).minute(0), topSide: timesheetLogger.TOP_SIDE.A},
-    {moment: moment().hour(12).minute(0), topSide: timesheetLogger.TOP_SIDE.B},
+    {moment: moment().hour(11).minute(59), topSide: timesheetLogger.TOP_SIDE.B},
     {moment: moment().hour(13).minute(0), topSide: timesheetLogger.TOP_SIDE.BOTTOM},
     {moment: moment().hour(14).minute(0), topSide: timesheetLogger.TOP_SIDE.B},
     {moment: moment().hour(14).minute(0), topSide: timesheetLogger.TOP_SIDE.C},
+    {moment: moment().hour(15).minute(52), topSide: timesheetLogger.TOP_SIDE.D},
     {moment: moment().hour(16).minute(0), topSide: timesheetLogger.TOP_SIDE.A}
   ], '08:00 - 08:15 top side was MAIL\n' +
     '08:15 - 09:00 top side was A\n' +
@@ -169,6 +171,7 @@ describe('integration tests with temporary files', () => {
     '10:00 - 12:00 top side was A\n' +
     '12:00 - 13:00 top side was B\n' +
     '\n' +
-    '14:00 - 16:00 top side was C\n'
+    '14:00 - 15:54 top side was C\n' +
+    '15:54 - 16:00 top side was D\n'
   )
 })
